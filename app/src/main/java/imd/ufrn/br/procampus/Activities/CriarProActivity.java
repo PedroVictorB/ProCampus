4b9b166b3e59ec697560bdb09e64de5c0c44ef58
Brunno Moreira <brunnomoreira.silva@gmail.com>
2015-10-07 10:14:03 -0300

    modificando register activity

--- app/src/main/java/imd/ufrn/br/procampus/Activities/CriarProActivity.java
+++ app/src/main/java/imd/ufrn/br/procampus/Activities/CriarProActivity.java
@@ -2,6 +2,7 @@ package imd.ufrn.br.procampus.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,10 +10,15 @@ import imd.ufrn.br.procampus.R;
 
 public class CriarProActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_pro);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
     }
 
     @Override
