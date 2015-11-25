930b0aef35da5ad03f213a322433dfb803db4c2f
Brunno Moreira <brunnomoreira.silva@gmail.com>
2015-11-25 00:28:08 -0200

    Listando problemas do usuáiro

--- app/src/main/java/imd/ufrn/br/procampus/activities/UserProblemActivity.java
+++ app/src/main/java/imd/ufrn/br/procampus/activities/UserProblemActivity.java
@@ -1,6 +1,8 @@
 package imd.ufrn.br.procampus.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
@@ -10,25 +12,77 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import cz.msebera.android.httpclient.Header;
 import imd.ufrn.br.procampus.R;
 import imd.ufrn.br.procampus.adapters.UserProblemAdapter;
 import imd.ufrn.br.procampus.entities.Problem;
 import imd.ufrn.br.procampus.entities.User;
+import imd.ufrn.br.procampus.utils.RestClient;
 
 public class UserProblemActivity extends AppCompatActivity {
 
+    private static final String TAG = UserProblemActivity.class.getSimpleName();
+
     private static final int ACTION_REGISTER_PROBLEM = 1;
+
+    private RecyclerView recyclerView;
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_user_problem);
         initComponents();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+        loadUserProblems();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     private void initComponents() {
@@ -37,27 +91,11 @@ public class UserProblemActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.userProblemList);
+        recyclerView = (RecyclerView) findViewById(R.id.userProblemList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-
-        ArrayList<Problem> mDataset = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-
-            Problem problem = new Problem();
-            problem.setTitle("Poste apagado na parada de C&T");
-
-            long time = System.currentTimeMillis();
-            problem.setPostDate(new Date(time));
-
-            problem.setDescription("Um poste apagado está inibindo a permanencia de alunos no local");
-
-            mDataset.add(problem);
-        }
-
-        recyclerView.setAdapter(new UserProblemAdapter(mDataset));
+        sharedPreferences = this.getSharedPreferences(MainActivity.class.getCanonicalName(), Context.MODE_PRIVATE);
     }
 
     public void onClickHandler(View view) {
@@ -81,4 +119,43 @@ public class UserProblemActivity extends AppCompatActivity {
         }
     }
 
+    private void loadUserProblems () {
+        String userId = sharedPreferences.getString("proCampusUserId", "4");
+        //RequestParams params = new RequestParams();
+        //params.put("id", userId);
+        RestClient.get(getString(R.string.api_url) + "/problem/user/" + userId, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    ArrayList<Problem> mDataset = new ArrayList<>();
+                    JSONArray jsonArray = response.getJSONArray("problems");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonProblem = jsonArray.getJSONObject(i);
+
+                        Problem problem = new Problem();
+                        problem.setTitle(jsonProblem.getString("title"));
+
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                        String data = jsonProblem.getString("date");
+                        Date date = new java.sql.Date(formatter.parse(data).getTime());
+                        problem.setPostDate(date);
+
+                        problem.setDescription(jsonProblem.getString("description"));
+
+                        mDataset.add(problem);
+                    }
+                    recyclerView.setAdapter(new UserProblemAdapter(mDataset));
+                } catch (JSONException e) {
+                    Log.d(TAG, "loadUserProblems JSONException - " + e.getMessage());
+                } catch (ParseException e) {
+                    Log.d(TAG, "loadUserProblems ParseException - " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d(TAG, "loadUserProblems Request Error");
+            }
+        });
+    }
 }
--- app/src/main/java/imd/ufrn/br/procampus/utils/RestClient.java
+++ app/src/main/java/imd/ufrn/br/procampus/utils/RestClient.java
@@ -2,6 +2,7 @@ package imd.ufrn.br.procampus.utils;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 /**
@@ -11,6 +12,10 @@ public class RestClient {
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void get(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
         client.get(url, params, responseHandler);
     }
 
